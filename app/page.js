f5a4b25486d6c23645e88636f8fe38c804b10232
iwamizujosiah <iwamizujosiah@gmail.com>
2026-07-05 14:25:25 -0700

    Delete app/page.js

--- app/page.js
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ textAlign: 'center' }}>
-      <h1>IWAMIZU ATHLETIC PERFORMANCE</h1>
-      <p>Purpose. Performance. Progress.</p>
-      <button className="cta-button">Apply for Coaching</button>
-    </main>
-  );
-}
