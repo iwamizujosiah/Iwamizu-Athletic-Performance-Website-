1955b3763a3b31367caa5c0130d0ff52cc0911e8
iwamizujosiah <iwamizujosiah@gmail.com>
2026-07-05 15:05:20 -0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,9 @@
-import './globals.css';⁠
+import './globals.css';
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body style={{ margin: 0, padding: 0 }}>{children}</body>
     </html>
   );
 }
